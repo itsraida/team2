579c03b160ea033aaef78f01eac6b35e1fa19648
raida <raidaalbkar@gmail.com>
2023-10-08 15:30:57 +0300

    Initial Commit

---
+++ PresentMe/PresentMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentMeApp.swift
+//  PresentMe
+//
+//  Created by raida on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PresentMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
