4d36967831a36633baf1cbe0a4b3a55fef8c1c73
JR506 <jawaherh.0050@gmail.com>
2023-10-08 17:46:49 +0300

    new project

---
+++ SwiftUIViewJ.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewJ.swift
+//  PresentMe
+//
+//  Created by JRshaman on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIViewJ: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewJ()
+}
