ca1f48823c2228b10c47c031519790c54b7f8ef5
najdalshawi <najdalshawi2@gmail.com>
2023-10-09 15:22:13 +0300

    new

---
+++ SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  PresentMe
+//
+//  Created by najd alshawi on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
